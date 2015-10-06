b5a32dca77fc1b1682d05b1346cae12119d73f02
CBke <cbatdotcom@gmail.com>
2015-10-06 23:31:15 +0200

    first working graph

---
+++ README.md
@@ -0,0 +1,20 @@
+About
+-----
+
+Some fun with [https://github.com/mbostock/d3/wiki/Gallery]
+
+
+Installation
+-----------
+
+```
+npm install
+```
+Usage
+-----
+
+```
+[http://localhost:3000/data/?table=data]
+
+[http://localhost:3000/]
+```
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var sqlite3 = require('sqlite3');
 var app = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World!');
+app.use(express.static('files'));
+
+app.get('/data', function (req, res) {
+  var db = new sqlite3.Database('db/data.db');
+
+  var table = req.query.table;
+  var sql = "SELECT * FROM " + table + " LIMIT 10";
+
+  db.all(sql, function(err, rows) {
+       res.send(JSON.stringify(rows));
+  });
+
+  db.close();
 });
 
 var server = app.listen(3000, function () {
